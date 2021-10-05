675e42725888536d8bc034bc143237b02e0f3e25
shaka <jaherm65@gmail.com>
2021-10-05 13:55:59 +0600

    footer added

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import NavbarComp from './Components/NavbarComp';
+import Footer from './Components/Footer';
 
 function App() {
   return (
-    <div className="App">
-      
+    <div className="page-container">
       <NavbarComp/>
+      <div className="content-wrap">
 
+      </div>
+      <Footer/>
     </div>
   );
 }
--- src/Components/NavbarComp.js
+++ src/Components/NavbarComp.js
@@ -12,6 +12,7 @@ import Contact from './Contact';
 import About from './About'
 import Services from './Services';
 import NotFound from './NotFound';
+import Footer from './Footer';
 
 export default class NavbarComp extends Component {
     render() {
@@ -19,7 +20,7 @@ export default class NavbarComp extends Component {
             <Router>
                 <div>
 
-                    <Navbar bg="dark" variant={"dark"} expand="lg">
+                    <Navbar bg="danger" variant={"dark"} expand="lg">
                         <Navbar.Brand href="#">EduSkills</Navbar.Brand>
                         <Navbar.Toggle aria-controls="navbarScroll" />
                         <Navbar.Collapse id="navbarScroll">
@@ -30,8 +31,8 @@ export default class NavbarComp extends Component {
                             >
                                 <Nav.Link as={Link} to="/home">Home</Nav.Link>
                                 <Nav.Link as={Link} to="/services">Services</Nav.Link>
-                                <Nav.Link as={Link} to="/about">About</Nav.Link>
-                                <Nav.Link as={Link} to="/contact">Contact Us</Nav.Link>
+                                <Nav.Link as={Link} to="/about">About Us</Nav.Link>
+                                <Nav.Link as={Link} to="/contact">Contact</Nav.Link>
 
                             </Nav>
 
@@ -49,7 +50,10 @@ export default class NavbarComp extends Component {
                         <Route path="/home">
                             <Home />
                         </Route>
-                        <Route exact path="/services">
+                        <Route exact path="/">
+                            <Home />
+                        </Route>
+                        <Route path="/services">
                             <Services />
                         </Route>
                         <Route path="*">
